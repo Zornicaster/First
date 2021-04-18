1de4b71a3c5b1153fcbb26cc7b9c1afab906f316
Zornicaster <82757431+Zornicaster@users.noreply.github.com>
2021-04-18 15:08:27 +0300

    Add files via upload

---
+++ First.js
@@ -0,0 +1,9 @@
+
+function submitForm(){
+    
+    let subject = document.getElementById("subject");
+    let email = document.getElementById("email");
+  
+    alert("Your email has been sent to Zornica!")
+
+}
---
+++ mypick.js
@@ -0,0 +1,49 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+modal.onclick = function() {
+    if(this.style.display === 'block') {
+       this.style.display = 'none' 
+    }
+};
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var img2 = document.getElementById("id2");
+var img3 = document.getElementById("id3");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+
+
+function displayModal(src, desc) {
+    modal.style.display = "block";
+    modalImg.src = src;
+    captionText.innerHTML = desc;
+}
+
+img2.onclick = function () {
+    displayModal(this.src, this.alt);
+}
+img.onclick = function () {
+    displayModal(this.src, this.alt);
+}
+img3.onclick = function () {
+    displayModal(this.src, this.alt);
+} 
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+function submitForm(){
+    
+    let subject = document.getElementById("subject");
+  
+    alert("Thank you for voting!")
+}
